119bc933fd04766da460ccd50137c424b295f236
Wesley Bruno <wesleybruno910@gmail.com>
2024-02-02 12:12:40 -0300

    feat: add Vehicle Model Interface

---
+++ src/app/model/vehicle.ts
@@ -0,0 +1,11 @@
+export interface IVehicle {
+    created_at: string;
+    user_id: number;
+    vehicle_type: string;
+    id: number;
+    name: string;
+    model: string;
+    year: number;
+    brand: string;
+    price: number;
+}
