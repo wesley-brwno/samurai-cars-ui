ea19bea3ab55949dd13f06bdf414d11577cb13e3
Wesley Bruno <wesleybruno910@gmail.com>
2024-02-02 12:15:06 -0300

    feat: map new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { AdminComponent } from './pages/admin/admin.component';
+import { AddVehicleComponent } from './pages/add-vehicle/add-vehicle.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
-  { path: "admin", component: AdminComponent }
+  { path: "admin", component: AdminComponent },
+  { path: "add-vehicle", component: AddVehicleComponent }
 ];
 
 @NgModule({
