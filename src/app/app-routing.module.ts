c7d5b60f43a61155695f3fb351143f1603706a0b
Wesley Bruno <wesleybruno910@gmail.com>
2024-03-15 19:43:12 -0300

    feat: add routing to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { AdminComponent } from './pages/admin/admin.component';
 import { AddVehicleComponent } from './pages/add-vehicle/add-vehicle.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+  { path: "", component: LandingComponent },
   { path: "login", component: LoginComponent },
   { path: "admin", component: AdminComponent },
+  { path: "home", component: HomeComponent },
   { path: "add-vehicle", component: AddVehicleComponent }
 ];
 
