951cef327035c398e2e8b5f83cb1694a9ecb8c13
Wesley Bruno <wesleybruno910@gmail.com>
2024-03-24 21:13:59 -0300

    Refactor: add modal to exib contact form

--- src/app/componentes/vehicle-card/vehicle-card.component.ts
+++ src/app/componentes/vehicle-card/vehicle-card.component.ts
@@ -1,24 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { VehicleData } from 'src/app/model/vehiclePage';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'vehicle-card',
   templateUrl: './vehicle-card.component.html',
   styleUrls: ['./vehicle-card.component.css']
 })
-export class VehicleCardComponent implements OnInit {
-loggedUserId: any;
-onShowContactForm(_t4: VehicleData) {
-throw new Error('Method not implemented.');
-}
-onSeeMoreClick(arg0: number) {
-throw new Error('Method not implemented.');
-}
-  @Input()
-  vehicleData!: VehicleData[]
+export class VehicleCardComponent {
 
-  ngOnInit(): void {
+  @Input()
+  vehicleData!: VehicleData[];
+  loggedUserId: any;
+  toggleContactForm: boolean = true;
+  @Output() contactSellerEmitter: EventEmitter<VehicleData> = new EventEmitter();
+
+  constructor(private router: Router, authService: AuthService) {
+    if (authService.isUserLogged()) {
+      this.loggedUserId = authService.getCurrentUser()?.id;
+    }
+  }
+
+  onSeeMoreClick(id: number) {
+    this.router.navigate([`vehicle-details/${id}`]);
+  }
+
+  onShowContactForm(_t4: VehicleData) {
     throw new Error('Method not implemented.');
+    // Todo
+  }
+
+  onContactClick(vehicle: VehicleData) {
+    this.contactSellerEmitter.emit(vehicle);
   }
 
 }
