0f12f20130deac166eaac24aade47eb10d6e4567
Wesley Bruno <wesleybruno910@gmail.com>
2024-03-25 16:28:56 -0300

    Feat: add service classes to make http connection with API's Messages controller

---
+++ src/app/services/message.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_URL } from '../utils/utils';
+import { catchError, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor(private http: HttpClient) { }
+
+  postContactMessage(formData: any) {
+    return this.http.post(`${API_URL}/messages`, formData).pipe(
+      map(response => response),
+      catchError((error: HttpErrorResponse) => {
+        throw new Error(error.error)
+      })
+    )
+  }
+}
