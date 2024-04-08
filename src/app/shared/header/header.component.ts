9a4d4eec3f79598c43f4405fc2a9cfd651ae89c7
Wesley Bruno <wesleybruno910@gmail.com>
2024-04-08 09:31:14 -0300

    feat: add funtionality to hide links when user is unauthenticated

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,23 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { API_URL } from 'src/app/utils/utils';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  swaggerUrl: string = `${API_URL}/swagger-ui/index.html`;
+  isUserLogged!: Boolean;
 
   constructor(public authenticationSevice: AuthService, private router: Router) {
-
   }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => this.isUserLogged = this.authenticationSevice.isUserLogged());
+  }
+
   onLogin() {
     this.router.navigate(['login']);  
   }
 
   onLogout() {
     this.authenticationSevice.logout();
-    this.router.navigate(['home']);
+    this.router.navigate(['']);
   }
 }
