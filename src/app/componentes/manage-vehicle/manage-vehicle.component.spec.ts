7e9cdb06e7acebf6cb4ac3459c0be7fba507f262
Wesley Bruno <wesleybruno910@gmail.com>
2024-05-01 09:56:50 -0300

    feat: add component to manage vehiclels

---
+++ src/app/componentes/manage-vehicle/manage-vehicle.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ManageVehicleComponent } from './manage-vehicle.component';
+
+describe('ManageVehicleComponent', () => {
+  let component: ManageVehicleComponent;
+  let fixture: ComponentFixture<ManageVehicleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ManageVehicleComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ManageVehicleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
