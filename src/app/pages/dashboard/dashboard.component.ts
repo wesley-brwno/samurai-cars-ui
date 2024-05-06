caf92b685cbf3c1a9bebf556b583546b8e2fabff
Wesley Bruno <wesleybruno910@gmail.com>
2024-05-06 16:39:14 -0300

    feat: add methods to retrieve messages and a method to open modal

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactMessage, contactMessagePage } from 'src/app/model/ContactMessagePage';
 import { AuthService, LoggedUser } from 'src/app/services/auth.service';
-import { VehicleService } from 'src/app/services/vehicle.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,17 +9,40 @@ import { VehicleService } from 'src/app/services/vehicle.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
   showVehicles: boolean = true;
   loggedUser!: LoggedUser | null;
+  messagesPage!: contactMessagePage;
+  selectedMessage!: ContactMessage;
 
-  constructor(private authService: AuthService, private vehicleService: VehicleService) {}
+  constructor(private authService: AuthService, private messagesService: MessageService) {}
   
   ngOnInit(): void {
     this.loggedUser = this.authService.getCurrentUser();
+    this.getContactMessages();
   }
 
   isUserAdmin(): boolean {
     return this.loggedUser?.roles.includes("ADMIN", 0) ? true : false;
+  }
+
+  getContactMessages() {
+    this.messagesService.getContactMessage("?page=0&size=10").subscribe({
+      next: (response) => {
+        this.messagesPage = response;        
+      },
+      error: (error) => {
+        alert("Fail to retrieve messages " + error)
+      } 
+    })
+  }
+
+  onSelectMessage(message: ContactMessage) {
+    this.selectedMessage = message;
+    this.showMessageModal();
+  }
+
+  showMessageModal() {
+    const modal = document.getElementById("message-modal") as HTMLDialogElement;
+    modal.showModal();
   }
 }
