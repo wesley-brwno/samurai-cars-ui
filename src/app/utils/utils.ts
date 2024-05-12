24ee742c0065f80d0c3d9f2ed11f2408ce8e9445
Wesley Bruno <wesleybruno910@gmail.com>
2024-05-11 22:34:24 -0300

    refactor: updates API url

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,3 +1,3 @@
-export const API_URL = "https://samuraicars-lx2l84u8.b4a.run"
-//export const API_URL = "https://samurai-cars.onrender.com"
+//export const API_URL = "https://samuraicars-lx2l84u8.b4a.run"
+export const API_URL = "https://samurai-cars.onrender.com/api/v2"
 //export const API_URL = "http://localhost:8080"
