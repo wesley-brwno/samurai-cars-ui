01da98f70285c0416694d0cc7785277d27933865
Wesley Bruno <wesleybruno910@gmail.com>
2024-06-12 13:45:12 -0300

    Refactor: Add clause to if statment to check if jwt token is expired before adding it to http header

--- src/app/security/auth.intercepeter.ts
+++ src/app/security/auth.intercepeter.ts
@@ -1,14 +1,17 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { AuthService } from "../services/auth.service";
 
 @Injectable({
     providedIn: 'root'
   })
 export class AuthIntercepterService implements HttpInterceptor { 
+  constructor(private authService: AuthService ){}
+  
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let tokenJwt = localStorage.getItem("tokenJwt");
-    if(tokenJwt) {
+    if(tokenJwt && this.authService.isUserLogged()) {
       req = req.clone({
         setHeaders : {
           Authorization: "Bearer "+tokenJwt
